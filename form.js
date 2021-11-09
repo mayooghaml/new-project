8090ac070f570e954551dd0b0be39a398f162ce4
mayooghaml <mayooghamuraleedharan@gmail.com>
2021-11-09 20:44:45 +0530

    new

--- form.js
+++ form.js
@@ -6,15 +6,25 @@ function validate(){
   if(regexp.test(email.value))
   {
     
-  error.innerHTML="valid";
-  error.style.color="green";
+  // error.innerHTML="valid";
+  // error.style.color="green";
   return true;
 }
 else{
 
-    error.innerHTML=("invalid");
-    error.style.color="red";
+    error.innerHTML=("please enter a valid email address!!");
+    error.style.color="black";
+    error.style.border="red solid 1px"
+    error.style.background="red"
+
 
     return false;
 }
+else
 }
+
+
+// The strong and weak password Regex pattern checker
+
+// let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
+// let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))')
